4708a2114c56199b35ae5a873bcf30f4016c0f21
Taka41802 <p280132a@gmail.com>
2024-12-03 11:11:39 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '41802' && password == '41802'
+    end
+  end
 end
