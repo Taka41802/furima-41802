bedf374e0da5fddef698e065358c556bef0612ad
Taka41802 <p280132a@gmail.com>
2024-12-03 23:51:46 +0900

    バリデーションとアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_ruby, presence: true
+  validates :first_name_ruby, presence: true
+  validates :date_of_birth, presence: true
+
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]+\z/, message: 'は半角英数字混合で入力してください' }
+
+  has_many :items
+  has_many :purchases
 end
