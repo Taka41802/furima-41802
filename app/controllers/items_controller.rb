9ac27ea888e3ddf60844183ba344d3955d991c18
Taka41802 <p280132a@gmail.com>
2024-12-05 11:28:09 +0900

    Active Storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :content, :category_id, :condition_id, :burden_id,
+                                 :area_id, :number_of_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
