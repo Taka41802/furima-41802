bcca643209c8e2ad4c25e249a45dc016eaf475a2
Taka41802 <p280132a@gmail.com>
2024-12-06 09:51:12 +0900

    商品投稿単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name              { Faker::Commerce.product_name }
+    content           { Faker::Lorem.sentence }
+    category_id       { 2 }
+    condition_id      { 2 }
+    burden_id         { 2 }
+    area_id           { 2 }
+    number_of_day_id  { 2 }
+    price             { Faker::Number.between(from: 300, to: 9_999_999) }
+    after(:build) do |item|
+      item.image.attach(io: File.open(Rails.root.join('app/assets/images/item-sample.png')), filename: 'item-sample.png')
+    end
   end
 end
