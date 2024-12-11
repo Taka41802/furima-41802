64caee89628284920396ea4be8243020f77dd572
Taka41802 <p280132a@gmail.com>
2024-12-11 16:10:25 +0900

    クレジットカード機能導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,14 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_form_params)
-    if @order_form.save
+    if @order_form.valid?
+      Payjp.api_key = 'sk_test_7ce2cfc5c138faaca0496cc6' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price, # 商品の値段
+        card: order_form_params[:token], # カードトークン
+        currency: 'jpy' # 通貨の種類（日本円）
+      )
+      @order_form.save
       redirect_to root_path
     else
       render :index, status: :unprocessable_entity
@@ -18,8 +25,8 @@ class OrdersController < ApplicationController
   private
 
   def order_form_params
-    params.require(:order_form).permit(:post_code, :area_id, :municipality, :street_address, :building_name, :phone_number, :token).merge(
-      user_id: current_user.id, item_id: params[:item_id]
+    params.require(:order_form).permit(:post_code, :area_id, :municipality, :street_address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
   end
 end
