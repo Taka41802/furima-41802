5525963d427320ff13143c9a1afdbaba47547ba5
Taka41802 <p280132a@gmail.com>
2024-12-13 10:18:44 +0900

    販売金額の修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -7,7 +7,7 @@ const price = () => {
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
 
     const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = Math.floor(inputValue - inputValue * 0.1);
+    profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
   });
 };
 
